acec208e05f98e9bbfdba397def08395ab909e1e
Pac <debianunix6@gmail.com>
2022-07-22 08:48:31 +0200

    transfert

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,16 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
-use core::fmt::Write;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Utopac OS demarrer");
 
-    #[cfg(test)]
-    test_main();
     loop {}
-}
-
-#[test_case]
-fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
 }
 
 
@@ -33,10 +22,10 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 
-#[cfg(test)]
+/*#[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
     println!("Demarrage de {} tests", tests.len());
     for test in tests {
         test();
     }
-}
+}*/
